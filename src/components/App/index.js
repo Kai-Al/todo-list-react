31ecef2038c882582b4b9fe64e4c05aef2dc42fb
Kai-Al <Kai.albornoz@udea.edu.co>
2022-07-30 17:48:48 -0500

    Guardado en localStorage

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -8,7 +8,17 @@ const defaultTodos = [
 ];
 
 function App() {
-  const [todos, setTodos] = React.useState(defaultTodos);
+  const localStorageTodos = localStorage.getItem('TODOS_V1');
+  let parsedTodos;
+
+  if (localStorageTodos) {
+    parsedTodos = JSON.parse(localStorageTodos);
+  } else {
+    localStorage.setItem('TODOS_V1', JSON.stringify([]));
+    parsedTodos = defaultTodos;
+  }
+
+  const [todos, setTodos] = React.useState(parsedTodos);
   const [searchValue, setSearchValue] = React.useState('');
 
   const completedTodos = todos.filter(todo => !!todo.completed).length;
@@ -26,18 +36,24 @@ function App() {
     });
   }
 
+  const saveTodos = (newTodos) => {
+    const stringfiedTodos = JSON.stringify(newTodos);
+    localStorage.setItem('TODOS_V1', stringfiedTodos);
+    setTodos(newTodos);
+  }
+
   const completeTodo = (text) => {
     const todoIndex = todos.findIndex(todo => todo.text === text);
     const newTodos = [...todos];
     newTodos[todoIndex].completed = true;
-    setTodos(newTodos);
+    saveTodos(newTodos);
   };
 
   const deleteTodo = (text) => {
     const todoIndex = todos.findIndex(todo => todo.text === text);
     const newTodos = [...todos];
     newTodos.splice(todoIndex, 1);
-    setTodos(newTodos);
+    saveTodos(newTodos);
   };
   
   return (
