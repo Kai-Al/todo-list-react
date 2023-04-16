2616622a808719621b545bf44a8fb7e58999e77d
Kai-Al <Kai.albornoz@udea.edu.co>
2023-04-16 12:12:30 -0500

    Feat: Creación del test de "hacer click" al botón de crear evento.

--- src/components/createTodoButton/index.test.js
+++ src/components/createTodoButton/index.test.js
@@ -1,11 +1,18 @@
 import "@testing-library/jest-dom/extend-expect";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import { CreateTodoButton } from "./index";
 import { screen } from "@testing-library/dom";
 
-describe("El botón de añadir un todo nuevo existe", () => {
+describe("<CreateTodoButton />", () => {
   test("Se renderiza el botón", () => {
     render(<CreateTodoButton />)
     screen.getByText("+");
+  });
+  test("El botón llama la función de hacer click", () => {
+    const mockSetOpenModal = jest.fn();
+    render(<CreateTodoButton setOpenModal = {mockSetOpenModal}/>)
+    const button = screen.getByText("+");
+    fireEvent.click(button);
+    expect(mockSetOpenModal).toHaveBeenCalledTimes(1);
   });
 });
